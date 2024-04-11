591b93a5af5ed68f9c53b6ddf28520861c6ba423
chiragjain307 <chiragjain.jain7@gmail.com>
2024-04-11 22:16:34 +0530

    drag and drop todos to reorder

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import TodoItems from './Components/TodoItems'
 
 function App() {
   const [todos, setTodo] = useState([])
+  const [activeTodo, setActiveTodo] = useState(null)
 
   const addTodo = (todo)=>setTodo((prev)=>[{id: Date.now(), ...todo}, ...prev])
   const updateTodo = (id, todo)=>setTodo((prev)=>prev.map((each)=>(each.id === id ? todo : each)))
@@ -30,14 +31,14 @@ function App() {
     <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}} >
       <div className="bg-[#172842] min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
-                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos - {activeTodo}</h1>
                     <div className="mb-4">
                         <TodoForm/>
                     </div>
                     <div className="flex flex-wrap gap-y-3">
                         {todos.map((todo)=>(
                           <div key={todo.id} className='w-full'>
-                            <TodoItems todo={todo}/>
+                            <TodoItems todo={todo} setActiveTodo={setActiveTodo}/>
                           </div>
                         ))}
                     </div>
--- src/Components/TodoItems.jsx
+++ src/Components/TodoItems.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import useTodo from '../Context/TodoContext';
 
-function TodoItems({ todo }) {
+function TodoItems({ todo, setActiveTodo }) {
     const [todoMsg, setTodoMsg] = useState(todo.todo)
     const [isTodoEditable, setIsTodoEditable] = useState(false)
     const { updateTodo, deleteTodo, toggleComplete } = useTodo()
@@ -19,9 +19,11 @@ function TodoItems({ todo }) {
 
     return (
         <div
-            className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
-                todo.completed ? "bg-[#c6e9a7]" : "bg-[#ccbed7]"
-            }`}
+            draggable={isTodoEditable ? "false" : "true"}
+            onDragStart={() => setActiveTodo(todo.id)}
+            onDragEnd={() => setActiveTodo(null)}
+            className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${todo.completed ? "bg-[#c6e9a7]" : "bg-[#ccbed7]"
+                }`}
         >
             <input
                 type="checkbox"
@@ -31,19 +33,18 @@ function TodoItems({ todo }) {
             />
             <input
                 type="text"
-                className={`border outline-none w-full bg-transparent rounded-lg ${
-                    isTodoEditable ? "border-black/10 px-2" : "border-transparent"
-                } ${todo.completed ? "line-through" : ""}`}
+                className={`border outline-none w-full bg-transparent rounded-lg ${isTodoEditable ? "border-black/10 px-2" : "border-transparent cursor-grab"
+                    } ${todo.completed ? "line-through" : ""}`}
                 value={todoMsg}
                 onChange={(e) => setTodoMsg(e.target.value)}
                 readOnly={!isTodoEditable}
             />
             {/* Edit, Save Button */}
             <button
-                className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 disabled:opacity-50"
+                className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 disabled:opacity-50 "
                 onClick={() => {
                     if (todo.completed) return;
-  
+
                     if (isTodoEditable) {
                         editTodo();
                     } else setIsTodoEditable((prev) => !prev);
@@ -61,6 +62,6 @@ function TodoItems({ todo }) {
             </button>
         </div>
     );
-  }
-  
-  export default TodoItems;
+}
+
+export default TodoItems;
