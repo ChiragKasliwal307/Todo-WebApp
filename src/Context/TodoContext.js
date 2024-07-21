0d288a0c673d8f314e4be521788a1797dba2d385
chiragjain307 <chiragjain.jain7@gmail.com>
2024-07-21 23:56:12 +0530

    formated

--- src/Context/TodoContext.js
+++ src/Context/TodoContext.js
@@ -1,20 +1,20 @@
 import { createContext, useContext } from "react";
 
 export const todoContext = createContext({
-    todos:[{
+    todos: [{
         id: 1,
         todo: "todo Msg",
         completed: false
 
     }],
-    addTodo:(todo)=>{},
-    updateTodo:(id, todo)=>{},
-    deleteTodo:(id)=>{},
-    toggleComplete:(id, completed)=>{}
+    addTodo: (todo) => { },
+    updateTodo: (id, todo) => { },
+    deleteTodo: (id) => { },
+    toggleComplete: (id, completed) => { }
 })
 
 export const TodoProvider = todoContext.Provider
 
-export default function useTodo(){
+export default function useTodo() {
     return useContext(todoContext)
 }
